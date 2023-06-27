82931b3a4cf94f6fa9af93afe739ee81d19a0d6c
AlexT-Dev <atacolmx@gmail.com>
2023-06-27 12:02:50 -0600

    Front end de newHistory al 80%

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -19,27 +19,66 @@ const doctorControl = {};
 //Desplegar los datos del paciente
 doctorControl.newHistory = (req, res) =>{
 
-    const { idpadactual } = req.params
+    const  idpadactual  = req.params.idpadactual;
+    const  status = req.params.status;
+    var ahf = null;
+    var app = null;
+    var parentesco = null;
+    var patientAHF = null;
+    var patientAPP = null;
+   
     //Para datos de la sesión
     const userAccess = req.session.user; 
     const userAccount = req.session.cuenta;
     const userStatus = req.session.status;
     const userType = req.session.userType;
     
-    //Se crea la conexión a la base de datos
+    req.getConnection((err,connu) =>{ //Busca los AHF del Catálogo
+      connu.query(`select * from ahf order by nombreahf`, (err, ahfs) =>{
+        if (err) { res.json(err) }
+          //Toma la vista de views
+          ahf = ahfs;
+         
+     })   
+   });
+
+  req.getConnection((err,connu) =>{ //Busca los Parentescos del Catálogo
+    connu.query(`select * from parentesco order by nombreparentesco`, (err, parentescos) =>{
+      if (err) { res.json(err) }
+        //Toma la vista de views
+        parentesco = parentescos;
+        
+    })   
+  });
+
+
+    
+   req.getConnection((err,connu) =>{ //Busca los Tipos de APP del Catálogo
+    connu.query("select tipoapp.idtipoapp, concat(nombretipoapp, ' - ',app.nombreapp) as nombretipo "
+              + "from app, tipoapp where app.idapp = tipoapp.idapp order by tipoapp.nombretipoapp", (err, apps) =>{
+      if (err) { res.json(err) }
+        app = apps;
+       
+   })   
+ });
+
     req.getConnection((err,conn) =>{
-      conn.query('select padecimiento.idpadactual, pacientes.nombrepaciente, pacientes.edad,pacientes.estadocivil, pacientes.escolaridad, pacientes.empleo, '
+      //Otiene los datos del paciente que será atendido
+      conn.query('select padecimiento.idpadactual, padecimiento.idpaciente, pacientes.nombrepaciente, pacientes.edad,pacientes.estadocivil, pacientes.escolaridad, pacientes.empleo, '
                + 'pacientes.lugarnacimiento, pacientes.lugarvive, pacientes.alergias from pacientes inner join padecimiento on pacientes.idpaciente = padecimiento.idpaciente '
                + 'where padecimiento.idpadactual = ?', [idpadactual], (err, patientData) =>{
          if (err) { res.json(err) }
               //Toma la vista de views
-            res.render('../views/doctors/ahfappHistory', {   //usa ../views/asssistant/asssistant porque es primera vez que entra a vistas
+            res.render('../views/doctors/ahfappHistory', {   //usa la vista para crear la historia clínica del paciente
              userAccess,
              userType,
-             patient: patientData,
+             ahfs: ahf,
+             parentescos: parentesco,
+             apps: app,
+             patient: patientData[0],
              title : "Paciente de Primera Vez"
             }) 
-         
+        
       })   
     })
  }
--- src/routes/doctor.js
+++ src/routes/doctor.js
@@ -5,14 +5,15 @@ const doctorController = require('../controllers/doctorController.js');
 //Se definen las rutas 
 
 //Para páginas en el módulo de citas
-router.get('/doctor/ahfapphistory/:idpadactual', doctorController.newHistory)       //Pantalla para agregar daos de ahf y app del paciente de PRIMERA VEZ
+router.get('/doctor/ahfapphistory/:idpadactual&:status', doctorController.newHistory)       //Pantalla para agregar daos de ahf y app del paciente de PRIMERA VEZ
 // router.get('/doctor/update/:idpadactual', doctorController.edit)       //Pantalla para modificar usuarios
 // router.get('/doctor/erase', doctorController.delete)                   //Pantalla para borrar la cita
 // router.get('/doctor/createPatient', doctorController.createPatient)    //Pantalla para crear nuevo paciente
 
 
 // //Para API
-// router.post('/doctor/addDate', doctorController.saveDate)             //Ruta para guardar nuevas citas 
+// router.post('/doctor/addahf', doctorController.saveAHF)             //Ruta para guardar nuevas citas 
+// router.post('/doctor/addapp', doctorController.saveAPP)             //Ruta para guardar nuevas citas 
 // router.post('/doctor/update/:idpadactual', doctorController.update)   //Ruta actualizar citas 
 // router.get('/doctor/delete/:idpadactual', doctorController.delete)    //Ruta para borrar citas (cambiar el estado)
 // router.post('/doctor/findDates', doctorController.findDates)          //Ruta para encontrar citas con fecha diferente (cambiar el estado)
